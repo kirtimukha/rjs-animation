e27d4f51414a0d730010a7067badd4a0f56f47ba
stella <stella.park.korea@gmail.com>
2023-01-19 00:38:09 +0900

    [U] git init

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ const GlobalStyle = createGlobalStyle`
   * {
     box-sizing: border-box;
 
-  }
+  } 
 
   html, body, div, span, applet, object, iframe,
   h1, h2, h3, h4, h5, h6, p, blockquote, pre,
